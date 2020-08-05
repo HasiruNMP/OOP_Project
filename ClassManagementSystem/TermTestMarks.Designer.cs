ca59ca5dca5baf10e41304679790ca84919b96bd
Rakshitha <rakshithadilshan1@gmail.com>
2020-08-05 22:30:25 +0530

    modified Markdetails

--- ClassManagementSystem/TermTestMarks.Designer.cs
+++ ClassManagementSystem/TermTestMarks.Designer.cs
@@ -28,135 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.Module1 = new System.Windows.Forms.Label();
-            this.Module2 = new System.Windows.Forms.Label();
-            this.Module3 = new System.Windows.Forms.Label();
-            this.textBoxModule1 = new System.Windows.Forms.TextBox();
-            this.textBoxModule2 = new System.Windows.Forms.TextBox();
-            this.textBoxModule3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.StudentextBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxModule3 = new System.Windows.Forms.TextBox();
+            this.textBoxModule2 = new System.Windows.Forms.TextBox();
+            this.textBoxModule1 = new System.Windows.Forms.TextBox();
+            this.Module3 = new System.Windows.Forms.Label();
+            this.Module2 = new System.Windows.Forms.Label();
+            this.Module1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.studentid1 = new System.Windows.Forms.TextBox();
+            this.getmark = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // groupBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(263, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Marks";
+            this.groupBox1.Controls.Add(this.StudentextBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxModule3);
+            this.groupBox1.Controls.Add(this.textBoxModule2);
+            this.groupBox1.Controls.Add(this.textBoxModule1);
+            this.groupBox1.Controls.Add(this.Module3);
+            this.groupBox1.Controls.Add(this.Module2);
+            this.groupBox1.Controls.Add(this.Module1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 577);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Exam Marks";
             // 
-            // Module1
+            // StudentextBox1
             // 
-            this.Module1.AutoSize = true;
-            this.Module1.Location = new System.Drawing.Point(111, 193);
-            this.Module1.Name = "Module1";
-            this.Module1.Size = new System.Drawing.Size(48, 13);
-            this.Module1.TabIndex = 2;
-            this.Module1.Text = "Module1";
-            // 
-            // Module2
-            // 
-            this.Module2.AutoSize = true;
-            this.Module2.Location = new System.Drawing.Point(111, 235);
-            this.Module2.Name = "Module2";
-            this.Module2.Size = new System.Drawing.Size(48, 13);
-            this.Module2.TabIndex = 3;
-            this.Module2.Text = "Module2";
-            // 
-            // Module3
-            // 
-            this.Module3.AutoSize = true;
-            this.Module3.Location = new System.Drawing.Point(111, 274);
-            this.Module3.Name = "Module3";
-            this.Module3.Size = new System.Drawing.Size(48, 13);
-            this.Module3.TabIndex = 4;
-            this.Module3.Text = "Module3";
-            // 
-            // textBoxModule1
-            // 
-            this.textBoxModule1.Location = new System.Drawing.Point(239, 186);
-            this.textBoxModule1.Name = "textBoxModule1";
-            this.textBoxModule1.Size = new System.Drawing.Size(185, 20);
-            this.textBoxModule1.TabIndex = 6;
-            // 
-            // textBoxModule2
-            // 
-            this.textBoxModule2.Location = new System.Drawing.Point(239, 228);
-            this.textBoxModule2.Name = "textBoxModule2";
-            this.textBoxModule2.Size = new System.Drawing.Size(185, 20);
-            this.textBoxModule2.TabIndex = 7;
-            // 
-            // textBoxModule3
-            // 
-            this.textBoxModule3.Location = new System.Drawing.Point(239, 274);
-            this.textBoxModule3.Name = "textBoxModule3";
-            this.textBoxModule3.Size = new System.Drawing.Size(185, 20);
-            this.textBoxModule3.TabIndex = 8;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(239, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 38);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Enter Marks";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.StudentextBox1.Location = new System.Drawing.Point(131, 53);
+            this.StudentextBox1.Name = "StudentextBox1";
+            this.StudentextBox1.Size = new System.Drawing.Size(185, 20);
+            this.StudentextBox1.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 146);
+            this.label2.Location = new System.Drawing.Point(6, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Student Name";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Student ID";
             // 
-            // StudentextBox1
+            // button1
             // 
-            this.StudentextBox1.Location = new System.Drawing.Point(239, 146);
-            this.StudentextBox1.Name = "StudentextBox1";
-            this.StudentextBox1.Size = new System.Drawing.Size(185, 20);
-            this.StudentextBox1.TabIndex = 5;
+            this.button1.Location = new System.Drawing.Point(131, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 38);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Enter Marks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxModule3
+            // 
+            this.textBoxModule3.Location = new System.Drawing.Point(131, 181);
+            this.textBoxModule3.Name = "textBoxModule3";
+            this.textBoxModule3.Size = new System.Drawing.Size(185, 20);
+            this.textBoxModule3.TabIndex = 18;
+            // 
+            // textBoxModule2
+            // 
+            this.textBoxModule2.Location = new System.Drawing.Point(131, 135);
+            this.textBoxModule2.Name = "textBoxModule2";
+            this.textBoxModule2.Size = new System.Drawing.Size(185, 20);
+            this.textBoxModule2.TabIndex = 17;
+            // 
+            // textBoxModule1
+            // 
+            this.textBoxModule1.Location = new System.Drawing.Point(131, 93);
+            this.textBoxModule1.Name = "textBoxModule1";
+            this.textBoxModule1.Size = new System.Drawing.Size(185, 20);
+            this.textBoxModule1.TabIndex = 16;
+            // 
+            // Module3
+            // 
+            this.Module3.AutoSize = true;
+            this.Module3.Location = new System.Drawing.Point(3, 181);
+            this.Module3.Name = "Module3";
+            this.Module3.Size = new System.Drawing.Size(48, 13);
+            this.Module3.TabIndex = 14;
+            this.Module3.Text = "Module3";
+            // 
+            // Module2
+            // 
+            this.Module2.AutoSize = true;
+            this.Module2.Location = new System.Drawing.Point(3, 142);
+            this.Module2.Name = "Module2";
+            this.Module2.Size = new System.Drawing.Size(48, 13);
+            this.Module2.TabIndex = 13;
+            this.Module2.Text = "Module2";
+            // 
+            // Module1
+            // 
+            this.Module1.AutoSize = true;
+            this.Module1.Location = new System.Drawing.Point(3, 100);
+            this.Module1.Name = "Module1";
+            this.Module1.Size = new System.Drawing.Size(48, 13);
+            this.Module1.TabIndex = 12;
+            this.Module1.Text = "Module1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.getmark);
+            this.groupBox2.Controls.Add(this.studentid1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(478, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(692, 596);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Marks Details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // studentid1
+            // 
+            this.studentid1.Location = new System.Drawing.Point(113, 52);
+            this.studentid1.Name = "studentid1";
+            this.studentid1.Size = new System.Drawing.Size(181, 20);
+            this.studentid1.TabIndex = 1;
+            // 
+            // getmark
+            // 
+            this.getmark.Location = new System.Drawing.Point(113, 100);
+            this.getmark.Name = "getmark";
+            this.getmark.Size = new System.Drawing.Size(85, 35);
+            this.getmark.TabIndex = 2;
+            this.getmark.Text = "Get Marks";
+            this.getmark.UseVisualStyleBackColor = true;
+            this.getmark.Click += new System.EventHandler(this.getmark_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(656, 310);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete Record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(333, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Total and Average";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // TermTestMarks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.StudentextBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBoxModule3);
-            this.Controls.Add(this.textBoxModule2);
-            this.Controls.Add(this.textBoxModule1);
-            this.Controls.Add(this.Module3);
-            this.Controls.Add(this.Module2);
-            this.Controls.Add(this.Module1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1217, 656);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "TermTestMarks";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label Module1;
-        private System.Windows.Forms.Label Module2;
-        private System.Windows.Forms.Label Module3;
-        private System.Windows.Forms.TextBox textBoxModule1;
-        private System.Windows.Forms.TextBox textBoxModule2;
-        private System.Windows.Forms.TextBox textBoxModule3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox StudentextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxModule3;
+        private System.Windows.Forms.TextBox textBoxModule2;
+        private System.Windows.Forms.TextBox textBoxModule1;
+        private System.Windows.Forms.Label Module3;
+        private System.Windows.Forms.Label Module2;
+        private System.Windows.Forms.Label Module1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button getmark;
+        private System.Windows.Forms.TextBox studentid1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
