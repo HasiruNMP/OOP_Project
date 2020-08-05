f7ab6c6228c8c427800014aea3d6bffd1dcec20e
Rakshitha <rakshithadilshan1@gmail.com>
2020-08-05 23:00:08 +0530

    Datagridview for semesterfee

--- ClassManagementSystem/studentfee.cs
+++ ClassManagementSystem/studentfee.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace student_management_system_semester_fee
 {
@@ -39,6 +40,65 @@ namespace student_management_system_semester_fee
 
         private void Paid_CheckedChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PC\Documents\GitHub\OOP_Project\StudentMangementDB.mdf;Integrated Security=True;Connect Timeout=30");
+
+            String query = "Select DegreeProgram,Semester,Fee,PaymentStatu from SemesterFees where StudentNumber = '" + studentID.Text + "' ";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+            DataSet set = new DataSet();
+
+            adapter.Fill(set, "SemesterFees");
+            dataGridView1.DataSource = set.Tables["SemesterFees"];
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+
+            string paystat = "";
+            if (paid.Checked)
+            {
+                paystat = "Paid";
+            }
+            else if (notpaid.Checked)
+            {
+                paystat = "Not Paid";
+            }
+
+
+
+            //DB connection
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PC\Documents\GitHub\OOP_Project\StudentMangementDB.mdf;Integrated Security=True;Connect Timeout=30");
+
+            String query = "Insert into SemesterFees(StudentNumber,DegreeProgram,Semester,Fee,PaymentStatu) Values ('" +StudentNumber.Text + "','" + Degreecombo.SelectedItem + "','" + semestercombo.SelectedItem + "', '" + feecombo.SelectedItem + "','" + paystat+ "')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Record added successfully");
+
+
+            }
+
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
 
         }
     }
